5bb989df49ca5bd45968d142c252d730dfad0517
GIDO LEE <gido@GIDOui-Macmini.local>
2025-08-29 16:33:00 +0900

    fix

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // 여기에 본인의 Supabase API 정보를 입력하세요.
 const supabaseUrl = 'https://dchtynddqgoqqthiqihn.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjaHR5bmRkcWdvcXF0aGlxaWhuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjM1OTIxMSwiZXhwIjoyMDcxOTM1MjExfQ.WiitSTiil9SVrDmUpJ48cPVaHfvnnSae4Ok9nCdVM0U'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjaHR5bmRkcWdvcXF0aGlxaWhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzNTkyMTEsImV4cCI6MjA3MTkzNTIxMX0.auQ2t3Jv3AlX9Iu5v-F4WKyN8id1akll4xxakpihg4A'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
