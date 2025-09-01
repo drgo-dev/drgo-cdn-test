c11463a4136ff6bd9828d5b23d87221f138602a5
drgo-dev <devcon@drgo.pro>
2025-09-01 17:12:37 +0900

    목록화 변경

--- src/views/Signature.vue
+++ src/views/Signature.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { supabase } from '../lib/supabaseClient';
 
 const props = defineProps({
